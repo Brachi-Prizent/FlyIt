2e2333c1a55271c9f7326d94c4686d05fe9d3e58
Brachi <b@gmail.com>
2024-02-21 19:59:35 +0200

    basic project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Blog } from "../src/components/Blog";
+// import { Login } from "../src/components/LogIn";
+// import { User } from "../src/components/User";
+// import { Main } from "../src/components/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Blog from "./components/Blog";
+import { References } from "./components/References";
+import { About } from "./components/About";
+import { AddFlight } from "./components/Flights/AddFlight";
+import { Invitation } from "./components/Invitation";
+import { UpdateFlight } from "./components/Flights/UpdateFlight";
+import { Flights } from "./components/Flights/Flights";
+import { LogIn } from "./components/LogIn";
+//import { Home } from "@mui/icons-material";
+import { Home } from "./components/Home";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{display:"flex", flexDirection:"row"}}>
+      <BrowserRouter>
+        <Main />
+        <Routes>
+          <Route path="" element={<Home/>}></Route>
+          <Route path="LogIn" element={<LogIn />}></Route>
+          <Route path="Blog" element={<Blog />}></Route>
+          <Route path="References" element={<References />}></Route>
+          <Route path="About" element={<About />}></Route>
+          <Route path="Flights" element={<Flights />}></Route>
+          <Route path="Flights/UpdateFlight/:id" element={<UpdateFlight />}></Route>
+          <Route path="Flights/AddFlight" element={<AddFlight />}></Route>
+          <Route path="Invitation/:idPlace" element={<Invitation />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </div>
+    </>
   );
 }
 
